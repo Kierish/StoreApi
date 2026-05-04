b3a2ab8325cf29fe2e76c2425ceb86e65af6e4e2
Kierish <wowrok07@gmail.com>
2026-05-04 14:31:55 +0200

    chore(docker): optimize container build

---
+++ Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection")
+                                ?? Environment.GetEnvironmentVariable("ConnectionStrings:DefaultConnection");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                connectionString = "Server=fake;Database=fake;User Id=fake;Password=fake;TrustServerCertificate=True;";
+            }
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
